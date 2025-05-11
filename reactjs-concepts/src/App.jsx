1e2777ca2d5d47d46b8a7473ba44cb38c170ca72
Syed Nazeer <109338083+nazeer007@users.noreply.github.com>
2025-05-11 20:54:20 +0530

    Day2 Commit

---
+++ reactjs-concepts/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import ClassBasedComponent from './components/class-based-components';
+import FunctionalComponent from './components/functional-component';
+
+function App() {
+  return (
+    <div>
+      <h1>React Js Concepts</h1>
+      <ClassBasedComponent />
+      <FunctionalComponent />
+    </div>
+  );
+}
+
+export default App
---
+++ reactjs-concepts/src/components/class-based-components.jsx
@@ -0,0 +1,15 @@
+import { Component } from "react";
+
+
+
+class ClassBasedComponent extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Class based component</h3>
+            </div>
+        );
+    }
+}
+
+export default ClassBasedComponent;

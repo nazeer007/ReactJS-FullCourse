84d931a0c651e3e663eb158b2f54903a3aa6ba43
Syed Nazeer <109338083+nazeer007@users.noreply.github.com>
2025-05-26 12:58:16 +0530

    useFetch custom hook

--- react-routing-and-custom-hooks/src/pages/comments/index.jsx
+++ react-routing-and-custom-hooks/src/pages/comments/index.jsx
@@ -1,7 +1,26 @@
+import useFetch from "../../hooks/use-fetch";
 
 
 export default function CommentsList() {
+    const {data, loading} = useFetch('https://dummyjson.com/comments');
+    if (loading) return <h3>Fetching Comments! Please wait</h3>;
     return (
-        <h2>Comments List Page</h2>
+        <div>
+            <h2>Comments List Page</h2>
+            <ul>
+                {
+                    data?.comments?.length > 0
+                    ? data?.comments?.map((commentItem) => (
+                        <div>
+                            <label>{commentItem?.body}</label>
+                            <p>Likes: {commentItem?.likes}</p>
+                        </div>
+                    ))
+                    :null
+                }
+            </ul>
+        </div>
+        
+        
     );
 }
--- react-routing-and-custom-hooks/src/pages/recipes/index.jsx
+++ react-routing-and-custom-hooks/src/pages/recipes/index.jsx
@@ -1,15 +1,26 @@
 import { useLocation } from "react-router-dom";
-
+import useFetch from "../../hooks/use-fetch";
 
 export default function RecipeList() {
+  const location = useLocation();
+  console.log(location);
 
-    const location = useLocation();
-    console.log(location);
-    
+  const { data, loading } = useFetch("https://dummyjson.com/recipes");
+  if (loading) return <h3>Fetching recipes! Please wait</h3>;
 
-    return (
-        <div>
-            <h2>Recipe List Page</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>Recipe List Page</h2>
+      <ul>
+        {data?.recipes?.length > 0
+          ? data?.recipes?.map((recipeItem) => (
+              <div>
+                <img src={recipeItem?.image} alt={recipeItem?.name} />
+                <label>{recipeItem?.name}</label>
+              </div>
+            ))
+          : null}
+      </ul>
+    </div>
+  );
 }

13fcb0bfe758e115049ea1b5b137d8411966a6f3
Syed Nazeer <109338083+nazeer007@users.noreply.github.com>
2025-05-26 13:29:36 +0530

    useWindowResize custom hook

--- react-routing-and-custom-hooks/src/pages/recipes/index.jsx
+++ react-routing-and-custom-hooks/src/pages/recipes/index.jsx
@@ -1,16 +1,25 @@
 import { useLocation } from "react-router-dom";
 import useFetch from "../../hooks/use-fetch";
+import useWindowResize from "../../hooks/use-window-resize";
 
 export default function RecipeList() {
   const location = useLocation();
-  console.log(location);
+  //   console.log(location);
 
-  const { data, loading } = useFetch("https://dummyjson.com/recipes");
+  const { data, loading, error } = useFetch("https://dummyjson.com/recipes");
+  const windowSize = useWindowResize();
+
   if (loading) return <h3>Fetching recipes! Please wait</h3>;
 
   return (
     <div>
-      <h2>Recipe List Page</h2>
+      <h2 style={{ color: windowSize.width < 768 ? "red" : "black" }}>
+        Recipe List Page
+      </h2>
+      <h3>
+        Current window width is {windowSize.width} and Current height is{" "}
+        {windowSize.height}{" "}
+      </h3>
       <ul>
         {data?.recipes?.length > 0
           ? data?.recipes?.map((recipeItem) => (
